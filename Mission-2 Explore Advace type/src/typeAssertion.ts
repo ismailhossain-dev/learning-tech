ec7f639a2332ebfb09402c9756e53d5044a8b6d0
sabbir <programmarsabbir@gmail.com>
2026-05-05 22:44:00 +0800

    typescript

--- Mission-2 Explore Advace type/src/typeAssertion.ts
+++ Mission-2 Explore Advace type/src/typeAssertion.ts
@@ -1,5 +1,6 @@
 /**
  * type Assertion => mane holo typescript teke besi buja. mane holo amra typescript er age bole divo type ta ki hobe
+ * Roles=> Amra jdoi sure na hoy tahole amra typeAssertion use korbo na
  */
 
 //========for example========
@@ -16,17 +17,40 @@ anything = "Sabbir";
 
 //1kg = 1000 gram
 
-const kgToGMConverter = (input: string | number) => {
+const kgToGMConverter = (input: string | number): string | number | undefined => {
   if (typeof input === "number") {
     return input * 1000;
   } else if (typeof input === "string") {
     //[] mordome Destructuring korchi and split er mardome space cut korchi and array te convert korchi
     const [value] = input.split(" ");
-    return `Converted output is: ${Number(input) * 1000}`;
+    return `Converted output is: ${Number(value) * 1000}`;
   }
 };
 
 //kgToGmConverter ta ekbar number return korbe
 //KgToGmConverter ta ekbar string return korbe
-const result = kgToGMConverter(2);
-const result2 = kgToGMConverter("2 kg");
+const result = kgToGMConverter("2 kg") as string; //over confident hoye bole dilam eta string hobe karone amra jani function string return korbe
+const result2 = kgToGMConverter(2) as number; // eta o sem
+console.log(result);
+
+//amra try , catch use korle ekta error moder ekta message pai seta ts use korbo
+
+type CustomError = {
+  message: string;
+};
+try {
+} catch (error) {
+  //console.log(error.message);❌❌
+  console.log((error as CustomError).message); //amra use ekane je message takbe overconfident
+}
+
+//practice
+//over confident hoye type set kore divo
+
+//gram to convert kg
+const gramToCovertKg = (gram: number): number => {
+  return gram / 1000;
+};
+
+const output = gramToCovertKg(2000);
+console.log(output);
---
+++ Mission-2 Explore Advace type/src/typeInterface.ts
@@ -0,0 +1,18 @@
+//--Type Interface ta typeAssertion motho
+
+//type interface sekar age amra ekto typeAlias deke nei
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const user1: User = {
+  name: "sabbir",
+  age: 17,
+};
+
+const user2: User = {
+  name: "abir",
+  age: 15,
+};
