02ae15cc97cf7b12a5fa5ff2a6a90466726abbd7
sabbir <programmarsabbir@gmail.com>
2026-05-14 00:58:11 +0800

    complete-node-setup

---
+++ learning-tech/Mission2_Node.js_Express_Expart/learning-node-part6/src/server.ts
@@ -0,0 +1,12 @@
+import { createServer, Server, IncomingMessage, ServerResponse } from "http";
+
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  console.log(req.url);
+
+  res.write("Hello World");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
