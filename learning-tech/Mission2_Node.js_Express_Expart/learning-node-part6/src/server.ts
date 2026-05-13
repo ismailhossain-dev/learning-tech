a6d6c997ccecda66f4ab134d013ad8b551056970
sabbir <programmarsabbir@gmail.com>
2026-05-14 01:38:16 +0800

    almost complete v-2

--- learning-tech/Mission2_Node.js_Express_Expart/learning-node-part6/src/server.ts
+++ learning-tech/Mission2_Node.js_Express_Expart/learning-node-part6/src/server.ts
@@ -1,11 +1,11 @@
+//these are coming from node_modules=> @types=> http.d.ts
 import { createServer, Server, IncomingMessage, ServerResponse } from "http";
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  console.log(" ami req boltechi", req);
-
-  res.write("Hello World");
+  //==here you can see which route hit url===like /user /product
+  console.log(" ami req boltechi", req.url);
+  console.log("method", req.method); //post, get , delete , put/patch
 });
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-console.log("Hello");
