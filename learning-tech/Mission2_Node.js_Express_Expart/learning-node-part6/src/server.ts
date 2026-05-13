493359fd55d8514e853466794fcc85a9c98916ac
sabbir <programmarsabbir@gmail.com>
2026-05-14 01:15:05 +0800

    start-video-2

--- learning-tech/Mission2_Node.js_Express_Expart/learning-node-part6/src/server.ts
+++ learning-tech/Mission2_Node.js_Express_Expart/learning-node-part6/src/server.ts
@@ -1,12 +1,11 @@
 import { createServer, Server, IncomingMessage, ServerResponse } from "http";
-
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.url);
+  console.log(" ami req boltechi", req);
 
   res.write("Hello World");
-  res.end();
 });
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+console.log("Hello");
